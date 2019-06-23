666ff210a0ba830409a1d3c955628e4a72d46f5c
hahaname <hahaemail>
2019-06-23 09:52:27 +0800

    清空默认文件内容以及调整目录结构

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
 Vue.config.productionTip = false
 
+// 创建vue实例，把App根组件替换到这里
 new Vue({
   router,
   render: h => h(App)
